69cbf596f0bb297c02a7028a48bd98fbb2ce4ae0
Steven Boddez <stevenboddez@gmail.com>
2017-12-12 19:51:51 -0700

    Made code less bad

--- js/main.js
+++ js/main.js
@@ -1,55 +1,39 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    startCursorBlinking()
-    chooseBackgroundColour()
-});
-
 function startCursorBlinking() {
-    var isCursorVisbile = true
+    var isCursorVisible = true;
     setInterval(function() {
-        if (isCursorVisbile) {
-            $("#cursor").css("visibility", "hidden")
+        if (isCursorVisible) {
+            d3.select('#cursor').style('visibility', 'hidden');
         } else {
-            $("#cursor").css("visibility","visible")
+            d3.select('#cursor').style('visibility', 'visible');
         }
-        isCursorVisbile = !isCursorVisbile
-    }, 600)
+        isCursorVisible = !isCursorVisible;
+    }, 600);
+}
+
+var colours = [
+    {name: 'red', colour: 'rgb(232,50,35)'},
+    {name: 'yellow', colour: 'rgb(252,237,81)'},
+    {name: 'green', colour: 'rgb(118,250,70)'},
+    {name: 'blue', colour: 'rgb(93,140,247)'},
+];
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 function chooseBackgroundColour() {
-    var colour = Math.floor(Math.random() * (5 - 1) + 1); // four colours
-    console.log(colour); 
-    switch (colour) {
-        case 1: 
-            //RED
-            $("body").css("color", "#E83223"); 
-            $(".button").css({"color" : "#E83223", "border" : "1pt solid #E83223" }); 
-            $("body .hacked").addClass("redneon-text"); 
-            $("body .prompt").addClass("redneon-symbols"); 
-            $("body .cursor").addClass("redneon-symbols"); 
-            break; 
-        case 2: 
-            //YELLOW
-            $("body").css("color", "#FCED51");
-            $(".button").css({"color" : "#FCED51", "border" : "1pt solid #FCED51" });
-            $("body .hacked").addClass("yellowneon-text"); 
-            $("body .prompt").addClass("yellowneon-symbols"); 
-            $("body .cursor").addClass("yellowneon-symbols"); 
-            break; 
-        case 3: 
-            //GREEN
-            $("body").css("color", "#76FA46");
-            $(".button").css({"color" : "#76FA46", "border" : "1pt solid #76FA46" });
-            $("body .hacked").addClass("greenneon-text"); 
-            $("body .prompt").addClass("greenneon-symbols"); 
-            $("body .cursor").addClass("greenneon-symbols"); 
-            break; 
-        case 4: 
-            //BLUE
-            $("body").css("color", "#5d8cf7"); 
-            $(".button").css({"color" : "#5d8cf7", "border" : "1pt solid #5d8cf7" }); 
-            $("body .hacked").addClass("blueneon-text"); 
-            $("body .prompt").addClass("blueneon-symbols"); 
-            $("body .cursor").addClass("blueneon-symbols"); 
-            break; 
-    } 
+    var colour = colours[getRandomInt(0, 4)];
+
+    d3.select('body').style('color', colour.colour);
+    d3.select('.button')
+        .style('color', colour.colour)
+        .style('border', '1pt solid ' + colour.colour);
+    d3.select('body .hacked').classed(colour.name + 'neon-text', true);
+    d3.select('body .prompt').classed(colour.name + 'neon-symbols', true);
+    d3.select('body .cursor').classed(colour.name + 'neon-symbols', true);
 }
+
+startCursorBlinking();
+chooseBackgroundColour();
