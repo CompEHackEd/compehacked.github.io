3da66da2ad2aedc36308a110a8c963712be50825
Arjun Kalburgi <askalburgi@gmail.com>
2017-12-19 17:21:42 -0500

    design tweaks :)

--- js/main.js
+++ js/main.js
@@ -59,6 +59,8 @@ chooseBackgroundColour();
 
 d3.selectAll('p.question')
     .on('click', function() {
+        var question = d3.select(this); 
+        question.style('text-transform', question.style('text-transform') === 'uppercase' ? 'none' : 'uppercase'); 
         var answer = d3.select(this.parentNode).select('p.answer');
         answer.style('display', answer.style('display') === 'none' ? 'block' : 'none');
     });
